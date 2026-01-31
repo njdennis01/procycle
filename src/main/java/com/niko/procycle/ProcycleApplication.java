400dc1844d6a40cf8625aca43830d9f0ecb7b1ef
njdennis01 <njdennis01@bvsd.org>
2026-01-31 15:22:01 -0700

    Add files via upload

---
+++ src/main/java/com/niko/procycle/ProcycleApplication.java
@@ -0,0 +1,13 @@
+package com.niko.procycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niko/procycle/ProcycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niko.procycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
